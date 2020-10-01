fc01fd463021b44e96599ea9925242c380e0815c
Thibaut <thibaut.gobert@hotmail.com>
2020-10-01 20:26:14 +0200

    cleaned useless props, added openMenu and closeMenu methods

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,21 +1,47 @@
 <script>
-import Vue from 'vue';
-import VueNiceMenu from '@/vue-nice-menu.vue';
+    import Vue from 'vue';
+    import VueNiceMenu from '@/vue-nice-menu.vue';
 
-export default Vue.extend({
-  name: 'ServeDev',
-  components: {
-    VueNiceMenu
-  }
-});
+    export default Vue.extend({
+        name: 'ServeDev',
+        components: {
+            VueNiceMenu
+        }
+    });
 </script>
 
 <template>
-  <div id="app">
-    <vue-nice-menu
-            :menu-count="4"
-            :icon-class="['fas fa-info-circle', 'fas fa-info-circle', 'fas fa-info-circle', 'fas fa-info-circle']"
-            :menu-url-list="[{isUrl: false, url: '/'},{isUrl: false, url: '/'},{isUrl: false, url: '/'},{isUrl: false, url: '/'}]"
-    ></vue-nice-menu>
-  </div>
+    <div id="app">
+        <vue-nice-menu
+                :menu-count="4"
+                :items="[
+                    {
+                        url: '/',
+                        isRouterLink: false,
+                        iconClass: '',
+                        backgroundColor: null
+                    },
+                    {
+                        url: '/',
+                        isRouterLink: false,
+                        iconClass: '',
+                        backgroundColor: null
+                    },{
+                        url: '/',
+                        isRouterLink: false,
+                        iconClass: '',
+                        backgroundColor: null
+                    },
+                    {
+                        url: '/',
+                        isRouterLink: false,
+                        iconClass: '',
+                        backgroundColor: null
+                    },
+
+
+
+                ]"
+        ></vue-nice-menu>
+    </div>
 </template>
